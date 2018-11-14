9bfb9db59ed2d90719f28f7fd0f3a84e09fbc395
Shinichi Kogai <kogai0121@gmail.com>
2018-11-14 18:20:07 +0900

    transpiling

--- dist/main.js
+++ dist/main.js
@@ -98,8 +98,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "myFn", function() { return myFn; });
 /* harmony import */ var unfetch_polyfill__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! unfetch/polyfill */ "./node_modules/unfetch/polyfill/polyfill.mjs");
 
-const myFn = a => a * a;
-
+var myFn = function myFn(a) {
+  return a * a;
+};
 
 /***/ }),
 
@@ -116,7 +117,6 @@ __webpack_require__.r(__webpack_exports__);
 
 if (!self.fetch) self.fetch = _src_index_mjs__WEBPACK_IMPORTED_MODULE_0__["default"];
 
-
 /***/ }),
 
 /***/ "./node_modules/unfetch/src/index.mjs":
@@ -128,60 +128,56 @@ if (!self.fetch) self.fetch = _src_index_mjs__WEBPACK_IMPORTED_MODULE_0__["defau
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = (function(url, options) {
-	options = options || {};
-	return new Promise( (resolve, reject) => {
-		let request = new XMLHttpRequest();
+/* harmony default export */ __webpack_exports__["default"] = (function (url, options) {
+  options = options || {};
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+    request.open(options.method || 'get', url, true);
 
-		request.open(options.method || 'get', url, true);
+    for (let i in options.headers) {
+      request.setRequestHeader(i, options.headers[i]);
+    }
 
-		for (let i in options.headers) {
-			request.setRequestHeader(i, options.headers[i]);
-		}
+    request.withCredentials = options.credentials == 'include';
 
-		request.withCredentials = options.credentials=='include';
+    request.onload = () => {
+      resolve(response());
+    };
 
-		request.onload = () => {
-			resolve(response());
-		};
+    request.onerror = reject;
+    request.send(options.body || null);
 
-		request.onerror = reject;
-
-		request.send(options.body || null);
-
-		function response() {
-			let keys = [],
-				all = [],
-				headers = {},
-				header;
-
-			request.getAllResponseHeaders().replace(/^(.*?):[^\S\n]*([\s\S]*?)$/gm, (m, key, value) => {
-				keys.push(key = key.toLowerCase());
-				all.push([key, value]);
-				header = headers[key];
-				headers[key] = header ? `${header},${value}` : value;
-			});
-
-			return {
-				ok: (request.status/100|0) == 2,		// 200-299
-				status: request.status,
-				statusText: request.statusText,
-				url: request.responseURL,
-				clone: response,
-				text: () => Promise.resolve(request.responseText),
-				json: () => Promise.resolve(request.responseText).then(JSON.parse),
-				blob: () => Promise.resolve(new Blob([request.response])),
-				headers: {
-					keys: () => keys,
-					entries: () => all,
-					get: n => headers[n.toLowerCase()],
-					has: n => n.toLowerCase() in headers
-				}
-			};
-		}
-	});
+    function response() {
+      let keys = [],
+          all = [],
+          headers = {},
+          header;
+      request.getAllResponseHeaders().replace(/^(.*?):[^\S\n]*([\s\S]*?)$/gm, (m, key, value) => {
+        keys.push(key = key.toLowerCase());
+        all.push([key, value]);
+        header = headers[key];
+        headers[key] = header ? `${header},${value}` : value;
+      });
+      return {
+        ok: (request.status / 100 | 0) == 2,
+        // 200-299
+        status: request.status,
+        statusText: request.statusText,
+        url: request.responseURL,
+        clone: response,
+        text: () => Promise.resolve(request.responseText),
+        json: () => Promise.resolve(request.responseText).then(JSON.parse),
+        blob: () => Promise.resolve(new Blob([request.response])),
+        headers: {
+          keys: () => keys,
+          entries: () => all,
+          get: n => headers[n.toLowerCase()],
+          has: n => n.toLowerCase() in headers
+        }
+      };
+    }
+  });
 });
-
 
 /***/ })
 
